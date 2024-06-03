5a21bc757c3696a3ddebd705363742cad0fd5cb9
imer alejandro <consymer1@gmail.com>
2024-06-03 11:23:34 -0700

    se implemento el dibujado y movimento de la ficha en pantalla, falta pulir esta funcionalidad, y hacer que al confirmar las pociciones se quede dibjada la pieza en el tablero

--- game.py
+++ game.py
@@ -1,17 +1,13 @@
 import pygame
-
 import sys
-from movimientos import navigate_and_select_piece # Importar la función de game_module.py
+from movimientos import navigate_and_select_piece, draw_selected_piece
 
 # Inicializar pygame
 pygame.init()
 
 # Colores
 WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
-GRAY = (200, 200, 200)
-GREEN = (0, 255, 0)
 LIGHT_BLUE = (57, 106, 238)
 COLORS = {
     "red": (255, 0, 0),
@@ -28,22 +24,22 @@ title_font = pygame.font.Font(None, 30)
 # Estado de piezas de cada jugador para tener su forma y contador
 player_pieces = {
     "player1": {
-        "piece1": (6, [(0, 0)]),  # Pieza de 1 bloque con cantidad 6
-        "piece2": (7, [(0, 0), (1, 0)]),  # Pieza de 2 bloques con cantidad 7
-        "piece3": (8, [(0, 0), (0, 1), (1, 0), (1, 1)]),  # Pieza de 4 bloques
-        "piece4": (6, [(0, 0), (0, 1), (0, 2), (1, 2)]),  # Pieza de 4 bloques en L
-        "piece5": (4, [(0, 0), (0, 1), (0, 2), (1, 0)]),  # Otra pieza en L
-        "piece6": (4, [(0, 0), (0, 1), (1, 1), (1, 2)]),  # Pieza de 4 bloques en Z
-        "piece7": (5, [(0, 0), (0, 1), (0, 2), (0, 3)]),  # Pieza de 4 bloques en línea
+        "piece1": (6, [(0, 0)]),
+        "piece2": (7, [(0, 0), (1, 0)]),
+        "piece3": (8, [(0, 0), (0, 1), (1, 0), (1, 1)]),
+        "piece4": (6, [(0, 0), (0, 1), (0, 2), (1, 2)]),
+        "piece5": (4, [(0, 0), (0, 1), (0, 2), (1, 0)]),
+        "piece6": (4, [(0, 0), (0, 1), (1, 1), (1, 2)]),
+        "piece7": (5, [(0, 0), (0, 1), (0, 2), (0, 3)]),
     },
     "player2": {
-        "piece1": (6, [(0, 0)]),  # Pieza de 1 bloque con cantidad 6
-        "piece2": (7, [(0, 0), (1, 0)]),  # Pieza de 2 bloques con cantidad 7
-        "piece3": (8, [(0, 0), (0, 1), (1, 0), (1, 1)]),  # Pieza de 4 bloques
-        "piece4": (6, [(0, 0), (0, 1), (0, 2), (1, 2)]),  # Pieza de 4 bloques en L
-        "piece5": (4, [(0, 0), (0, 1), (0, 2), (1, 0)]),  # Otra pieza en L
-        "piece6": (4, [(0, 0), (0, 1), (1, 1), (1, 2)]),  # Pieza de 4 bloques en Z
-        "piece7": (5, [(0, 0), (0, 1), (0, 2), (0, 3)]),  # Pieza de 4 bloques en línea
+        "piece1": (6, [(0, 0)]),
+        "piece2": (7, [(0, 0), (1, 0)]),
+        "piece3": (8, [(0, 0), (0, 1), (1, 0), (1, 1)]),
+        "piece4": (6, [(0, 0), (0, 1), (0, 2), (1, 2)]),
+        "piece5": (4, [(0, 0), (0, 1), (0, 2), (1, 0)]),
+        "piece6": (4, [(0, 0), (0, 1), (1, 1), (1, 2)]),
+        "piece7": (5, [(0, 0), (0, 1), (0, 2), (0, 3)]),
     }
 }
 
@@ -56,87 +52,47 @@ def draw_text(text, font, color, surface, x, y, center=True):
         text_rect = text_obj.get_rect(topleft=(x, y))
     surface.blit(text_obj, text_rect)
 
-# Función para mostrar las piezas de cada jugador debajo de este con sus contadores
-def draw_pieces_norm(surface, pieces, x, y, color):
+def draw_pieces(surface, pieces, x, y, color, current_index, selected):
     piece_y = y
-    margin = 10  # Margen entre piezas
-    for piece_name, (count, cells) in pieces.items():
-        # Calcular el ancho de la pieza
+    margin = 10
+    for i, (piece_name, (count, cells)) in enumerate(pieces.items()):
         piece_width = max(cell[0] for cell in cells) + 1
-        # Dibujar la pieza
+        
+        alpha = 100 if count > 0 else 255
+        if i == current_index and selected:
+            alpha = 255
         for cell in cells:
             cell_rect = pygame.Rect(x + cell[0] * 20, piece_y + cell[1] * 20, 20, 20)
             pygame.draw.rect(surface, color, cell_rect)
             pygame.draw.rect(surface, (255, 255, 255), cell_rect, 1)
-        # Dibujar el número de piezas restantes
-        draw_text(str(count), font, BLACK, surface, x + piece_width * 20 + 20, piece_y + 8, center=False)
-        piece_y += (max(cell[1] for cell in cells) + 1) * 25 + margin
-
-def draw_pieces(surface, pieces, x, y, color, current_index, selected):
-    piece_y = y
-    margin = 10  # Margen entre piezas
-    for i, (piece_name, (count, cells)) in enumerate(pieces.items()):
-                # Calcular el ancho de la pieza
-        piece_width = max(cell[0] for cell in cells) + 1
-        
-        alpha = 100 if count > 0 else 255  # Opacidad para piezas seleccionables y no seleccionables
-        if i == current_index and selected:
-            alpha = 255  # Ajustar opacidad para la pieza seleccionada
-        for cell in cells:
-            cell_rect = pygame.Rect(x + cell[0] * 20, piece_y + cell[1] * 20, 20, 20)
-            pygame.draw.rect(surface, color, cell_rect)  # Dibujar la pieza
-            pygame.draw.rect(surface, (255, 255, 255), cell_rect, 1)  # Dibujar el borde
             if i == current_index and selected:
-                pygame.draw.rect(surface, GREEN, cell_rect, 3)  # Dibujar el borde verde para la pieza seleccionada
-        # Dibujar el número de piezas restantes
+                pygame.draw.rect(surface, (0, 255, 0), cell_rect, 3)
         draw_text(str(count), font, BLACK, surface, x + piece_width * 20 + 20, piece_y + 8, center=False)
-        piece_y += (max(cell[1] for cell in cells) + 1) * 25 + margin
-
-def draw_pieces_with_opacity(surface, pieces, x, y, color, current_index, selected):
-    piece_y = y
-    margin = 10  # Margen entre piezas
-    for i, (piece_name, (count, cells)) in enumerate(pieces.items()):
-        alpha = 100 if count > 0 else 255  # Opacidad para piezas seleccionables y no seleccionables
-        if i == current_index and selected:
-            alpha = 255  # Ajustar opacidad para la pieza seleccionada
-        for cell in cells:
-            cell_rect = pygame.Rect(x + cell[0] * 20, piece_y + cell[1] * 20, 20, 20)
-            pygame.draw.rect(surface, color + (alpha,), cell_rect)  # Agregar el canal alpha para la opacidad
-
-            # Dibujar borde verde si la pieza es seleccionable
-            if count > 0:
-                pygame.draw.rect(surface, BLUE, cell_rect, 5)
-            # Dibujar borde rojo si la pieza no es seleccionable
-            else:
-                pygame.draw.rect(surface, BLACK, cell_rect, 5)
-
         piece_y += (max(cell[1] for cell in cells) + 1) * 25 + margin
 
 def start_game(player1_name, player1_color, player2_name, player2_color):
-    # Configuración de la pantalla
     WIDTH, HEIGHT = 1000, 700
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Blokus - Python")
 
-    # Tamaño del tablero
     board_size = 20
     cell_size = 30
     board_origin = [WIDTH // 2 - board_size // 2 * cell_size, HEIGHT // 2 - board_size // 2 * cell_size]
 
-    # Inicialización del turno
     current_turn = "player1"
 
-    selected_piece_index = 0  # Variable para almacenar el índice de la pieza seleccionada
-    selected = False  # Variable para indicar si una pieza está seleccionada
+    selected_piece_index = 0
+    selected = False
+
+    piece_x, piece_y = 0, 0
 
     running = True
     while running:
         screen.fill(WHITE)
-        # Dibujar el tablero
         for row in range(board_size):
             for col in range(board_size):
                 rect = pygame.Rect(board_origin[0] + col * cell_size, board_origin[1] + row * cell_size, cell_size, cell_size)
-                pygame.draw.rect(screen, (0, 0, 0), rect, 1)
+                pygame.draw.rect(screen, BLACK, rect, 1)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -150,34 +106,43 @@ def start_game(player1_name, player1_color, player2_name, player2_color):
                     if count > 0:
                         player_pieces[current_turn][piece_name] = (count - 1, player_pieces[current_turn][piece_name][1])
                         selected = True
-                        current_turn = "player2" if current_turn == "player1" else "player1"  # Cambiar de turno
-                        selected_piece_index = 0  # Resetear el índice de la pieza seleccionada al cambiar de turno
-                    else:
-                        selected = False  # Deseleccionar la pieza si no hay suficientes disponibles
+                        piece_x, piece_y = 0, 0
 
-        # Ajustar la velocidad de desplazamiento del selector de ficha
-        pygame.time.Clock().tick(10)  # 10 FPS para un movimiento más suave
+                elif event.key == pygame.K_RETURN and selected:
+                    current_turn = "player2" if current_turn == "player1" else "player1"
+                    selected = False
+                    selected_piece_index = 0
 
-        # Dibujar las piezas con opacidad y seleccionar una pieza
-        draw_pieces_with_opacity(screen, player_pieces[current_turn], 50, 200, COLORS[player1_color], selected_piece_index, selected)
+                elif selected:
+                    if event.key == pygame.K_UP and piece_y > 0:
+                        piece_y -= 1
+                    elif event.key == pygame.K_DOWN and piece_y < board_size - 1:
+                        piece_y += 1
+                    elif event.key == pygame.K_LEFT and piece_x > 0:
+                        piece_x -= 1
+                    elif event.key == pygame.K_RIGHT and piece_x < board_size - 1:
+                        piece_x += 1
+
+        pygame.time.Clock().tick(10)
+
+        draw_pieces(screen, player_pieces[current_turn], 50, 200, COLORS[player1_color if current_turn == "player1" else player2_color], selected_piece_index, selected)
         selected_piece_index = navigate_and_select_piece(selected_piece_index, player_pieces[current_turn])
-        # Dibujar el título del juego
+
         draw_text("BLOKUS - Python", title_font, LIGHT_BLUE, screen, WIDTH // 2, 10)
         
-        # Dibujar el indicador de turno
         draw_text(f"Turno de: {player1_name if current_turn == 'player1' else player2_name}", font, BLACK, screen, WIDTH // 2, 30)
-
-        # Dibujar nombres de los jugadores
+       
         draw_text(player1_name, font, COLORS[player1_color], screen, 120, 140)
         draw_text(player2_name, font, COLORS[player2_color], screen, 920, 140)
+
+        if current_turn == 'player1':
+            draw_pieces(screen, player_pieces["player1"], 50, 200, COLORS[player1_color], selected_piece_index, selected)
+            draw_pieces(screen, player_pieces["player2"], 850, 200, COLORS[player2_color], selected_piece_index, False)
+        else:
+            draw_pieces(screen, player_pieces["player2"], 850, 200, COLORS[player2_color], selected_piece_index, selected)
+            draw_pieces(screen, player_pieces["player1"], 50, 200, COLORS[player1_color], selected_piece_index, False)
         
-        # Dibujar piezas de los jugadores
-        if  current_turn == 'player1':
-            draw_pieces(screen, player_pieces["player1"], 50, 200, COLORS[player1_color],selected_piece_index,selected)
-            draw_pieces_norm(screen, player_pieces["player2"], 850, 200, COLORS[player2_color])
-        elif current_turn == 'player2':
-            draw_pieces(screen, player_pieces["player2"],  850, 200, COLORS[player2_color],selected_piece_index,selected)
-            draw_pieces_norm(screen, player_pieces["player1"],50, 200, COLORS[player1_color])
+        if selected:
+            draw_selected_piece(screen, selected_piece_index, player_pieces[current_turn], piece_x, piece_y, cell_size, board_origin, COLORS[player1_color if current_turn == "player1" else player2_color])
 
-
-        pygame.display.flip()
+        pygame.display.update()
--- movimientos.py
+++ movimientos.py
@@ -2,13 +2,24 @@
 #la ficha en el tablero y sus contadores durante la partida 
 
 import pygame
-
 def navigate_and_select_piece(current_index, pieces):
-    keys = pygame.key.get_pressed()  # Obtener el estado de las teclas
+    keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_UP]:  # Si se presiona la tecla arriba
-        current_index = (current_index - 1) % len(pieces)  # Seleccionar la pieza anterior
-    elif keys[pygame.K_DOWN]:  # Si se presiona la tecla abajo
-        current_index = (current_index + 1) % len(pieces)  # Seleccionar la pieza siguiente
+    if keys[pygame.K_UP]:
+        current_index = (current_index - 1) % len(pieces)
+    elif keys[pygame.K_DOWN]:
+        current_index = (current_index + 1) % len(pieces)
 
     return current_index
+def draw_selected_piece(surface, current_index, pieces, piece_x, piece_y, cell_size, board_origin, player_color):
+    if current_index is not None:
+        piece_name = list(pieces.keys())[current_index]
+        _, cells = pieces[piece_name]
+
+        # Dibujar la pieza en el tablero
+        for cell in cells:
+            cell_rect = pygame.Rect(board_origin[0] + (piece_x + cell[0]) * cell_size,
+                                    board_origin[1] + (piece_y + cell[1]) * cell_size,
+                                    cell_size, cell_size)
+            pygame.draw.rect(surface, player_color, cell_rect)
+            pygame.draw.rect(surface, (0, 0, 0), cell_rect, 1)
